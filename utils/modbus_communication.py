0d230a2fedd55fa317d8b3c85f2fa60159ce658f
dreece2304 <dreece2304@gmail.com>
2024-08-02 11:34:50 -0700

    Updated register to adjust index by 1

--- utils/modbus_communication.py
+++ utils/modbus_communication.py
@@ -1,9 +1,12 @@
 import minimalmodbus
 import json
 import os
+import struct
+
 
 class ModbusCommunication:
-    def __init__(self, port='COM5', baudrate=115200, device_ids=[1, 2, 3], json_file='C:/Users/bergsman_lab_admin/PycharmProjects/ReactorControlApp/OMEGA_CN616A_Registers.json'):
+    def __init__(self, port='COM5', baudrate=115200, device_ids=[1, 2, 3],
+                 json_file='C:/Users/bergsman_lab_admin/PycharmProjects/ReactorControlApp/OMEGA_CN616A_Registers.json'):
         self.devices = {}
         self.load_registers(json_file)
         for device_id in device_ids:
@@ -21,7 +24,10 @@ class ModbusCommunication:
 
     def read_temperature_registers(self, device_id, base_register):
         # Read two consecutive 16-bit registers and combine them into a 32-bit value
-        msb_value = self.devices[device_id].read_register(base_register + 1, functioncode=3, signed=False)
-        lsb_value = self.devices[device_id].read_register(base_register + 2, functioncode=3, signed=False)
+        msb_value = self.devices[device_id].read_register(base_register, functioncode=3, signed=False)
+        lsb_value = self.devices[device_id].read_register(base_register + 1, functioncode=3, signed=False)
         combined_value = (msb_value << 16) | lsb_value
-        return combined_value
+
+        # Interpret the combined value as a 32-bit IEEE floating-point number
+        float_value = struct.unpack('>f', combined_value.to_bytes(4, byteorder='big'))[0]
+        return float_value
