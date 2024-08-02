169ad3d97c9013b043ecbe6e9de99071d48c9b5d
dreece2304 <dreece2304@gmail.com>
2024-08-02 11:43:54 -0700

    Updated reset device code

--- utils/modbus_communication.py
+++ utils/modbus_communication.py
@@ -23,7 +23,7 @@ class ModbusCommunication:
         print(f"Loaded registers: {self.registers.keys()}")  # Debugging output
 
     def read_temperature_registers(self, device_id, base_register):
-        # Read two consecutive 16-bit registers and combine them into a 32-bit value
+        # Read two consecutive 16-bit registers and combine them into a 32-bit IEEE float
         msb_value = self.devices[device_id].read_register(base_register, functioncode=3, signed=False)
         lsb_value = self.devices[device_id].read_register(base_register + 1, functioncode=3, signed=False)
         combined_value = (msb_value << 16) | lsb_value
@@ -31,3 +31,17 @@ class ModbusCommunication:
         # Interpret the combined value as a 32-bit IEEE floating-point number
         float_value = struct.unpack('>f', combined_value.to_bytes(4, byteorder='big'))[0]
         return float_value
+
+    def write_register(self, device_id, register_name, register_type, value):
+        register_info = self.registers.get(register_type)
+        if not register_info:
+            raise ValueError(f"Register type '{register_type}' not found in the JSON file.")
+
+        for reg in register_info:
+            if reg['Mnemonic'] == register_name:
+                register_address = int(reg['Index'], 16) + 1  # Adjust index for Modbus
+                self.devices[device_id].write_register(register_address, value,
+                                                       functioncode=6)  # Use function code 6 for writing a single register
+                return
+        raise ValueError(f"Register '{register_name}' not found in '{register_type}'.")
+
